8c518ba38c6ef3bc2aa8e27cc2fce5d0e780cca2
Alexander <sashacomp2@gmail.com>
2021-02-24 18:26:52 +0300

    Added a lifeTimer of player

---
+++ Assets/Scripts/GUI/LifeTime.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LifeTime : MonoBehaviour
+{
+    [SerializeField] private float lifeTimeOfPlayer = 300.0f;
+
+    [SerializeField] private int sceneNumber = 0;
+
+    void Update()
+    {
+        lifeTimeOfPlayer -= Time.deltaTime;
+        if (lifeTimeOfPlayer < 0)
+        {
+            SceneManager.LoadScene(sceneNumber);
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUI.Box(new Rect(0, 0, 100, 30), lifeTimeOfPlayer.ToString());
+    }
+}
