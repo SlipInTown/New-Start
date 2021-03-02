b0031913dbfc2b7f74c07cddc603b0315a8cf801
Alexander <sashacomp2@gmail.com>
2021-03-02 15:33:53 +0300

    4.3 task done
    Сделал еще одного бота, с blend tree который увеличивает значение float в аниматоре если я нахожусь близко к нему (через Time.DeltaTime и MAthf.Clamp). И наоборот если далеко уменьшает значение float

--- Assets/Scripts/BlendTreeControl/AIBlendTree.cs
+++ Assets/Scripts/BlendTreeControl/AIBlendTree.cs
@@ -1,15 +1,15 @@
-using System;
+﻿using System;
 using UnityEngine;
 
 namespace UnityStandardAssets.Characters.ThirdPerson
 {
-    public class AICharacterControl : MonoBehaviour
+    public class AIBlendTree : MonoBehaviour
     {
         public Transform target;
         [SerializeField] private Animator animatorAI;
         [SerializeField] private string playerTag = "Player";
         private RaycastHit hit;
-        
+        [SerializeField,Range(0f, 1f)] private float blend = 0f;
 
 
         private void Start()
@@ -27,25 +27,21 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             {
                 if (hit.collider.CompareTag(playerTag))
                 {
-                    if (hit.distance > 10)
+                    if (hit.distance < 10)
                     {
-                        animatorAI.SetBool("Run", true);
-                        animatorAI.SetBool("Walk", false);
+                        blend += (Time.deltaTime * 0.25f);
+                        animatorAI.SetFloat("Walk", Mathf.Clamp(blend, 0f, 1f));
                     }
                     else
                     {
-                        animatorAI.SetBool("Run", false);
-                        animatorAI.SetBool("Walk", true);
+                        blend -= (Time.deltaTime * 0.25f);
+                        animatorAI.SetFloat("Walk", Mathf.Clamp(blend, 0f, 1f));
                     }
                 }
-                else
-                {
-                    animatorAI.SetBool("Run", false);
-                    animatorAI.SetBool("Walk", false);
-                }
+                
                 Debug.DrawRay(transform.position, hitDirection * distanceOfRay, Color.red);
             }
-            
+
         }
 
     }
