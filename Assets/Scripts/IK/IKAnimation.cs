90b26f9d0605ab1072ca1b959db637b365afe05c
Alexander <sashacomp2@gmail.com>
2021-03-07 06:19:55 +0300

    BlendEthan IK and StateEthan Ragdoll

--- Assets/Scripts/IK/IKAnimation.cs
+++ Assets/Scripts/IK/IKAnimation.cs
@@ -3,15 +3,62 @@
 [RequireComponent(typeof(Animator))]
 public class IKAnimation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Animator animatorGO;
+    [SerializeField] private Transform LookObj;
+    [SerializeField] private Transform HandsObj;
+
+    [SerializeField] private Transform rightFoot;
+    [SerializeField] private Transform leftFoot;
+
+    [SerializeField, Range(0f, 1f)] private float handsWeight = 1;
+    [SerializeField, Range(0f, 1f)] private float lookWeight = 1;
+    [SerializeField] bool ikActive;
+    private RaycastHit rayHit;
+    private void OnAnimatorIK(int layerIndex)
     {
-        
+        if (Physics.Raycast(transform.position + Vector3.up, transform.forward, out rayHit,2f))
+        {
+            handsWeight = 1;
+            LookObj = HandsObj = rayHit.transform;
+        }
+        if (ikActive)
+        {
+            if (HandsObj)
+            {
+                SetWeight(handsWeight, AvatarIKGoal.LeftHand);
+                SetWeight(handsWeight,AvatarIKGoal.RightHand);
+
+                SetIKPos(HandsObj.position, AvatarIKGoal.RightHand);
+                SetIKRot(HandsObj.rotation, AvatarIKGoal.RightHand);
+
+                SetIKPos(HandsObj.position, AvatarIKGoal.LeftHand);
+                SetIKRot(HandsObj.rotation, AvatarIKGoal.LeftHand);
+            }
+            if (LookObj)
+            {
+                animatorGO.SetLookAtWeight(lookWeight);
+                animatorGO.SetLookAtPosition(LookObj.position);
+            }
+        }
+        else
+        {
+            SetWeight(0, AvatarIKGoal.RightHand);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetIKPos(Vector3 position, AvatarIKGoal body)
     {
-        
+        animatorGO.SetIKPosition(body, position);
+    }
+
+    private void SetIKRot(Quaternion rotation, AvatarIKGoal body)
+    {
+        animatorGO.SetIKRotation(body, rotation);
+    }
+
+    private void SetWeight(float weight, AvatarIKGoal body)
+    {
+        animatorGO.SetIKPositionWeight(body, weight);
+        animatorGO.SetIKRotationWeight(body, weight);
     }
 }
---
+++ Assets/Scripts/RagDoll/RagDollDIe.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class RagDollDIe : MonoBehaviour
+{
+    [SerializeField] private Rigidbody[] rigidBodies;
+    [SerializeField] private Collider[] colliders;
+    [SerializeField] private Animator animator;
+
+    [SerializeField] private float lifeTime = 6f;
+    private void Start()
+    {
+        if (!animator) animator = GetComponent<Animator>();
+        if (rigidBodies.Length == 0) rigidBodies = GetComponentsInChildren<Rigidbody>();
+        if (colliders.Length == 0) colliders = GetComponentsInChildren<Collider>();
+        AliveMake();
+    }
+
+    private void RagDollState(bool active)
+    {
+        animator.enabled = !active;
+        if (rigidBodies.Length != colliders.Length) return;
+        for (int i = 0; i < rigidBodies.Length; i++)
+        {
+            rigidBodies[i].isKinematic = !active;
+
+            colliders[i].enabled = active;
+        }
+    }
+
+    private void AliveState(bool active)
+    {
+        animator.enabled = active;
+        rigidBodies[0].isKinematic = !active;
+        colliders[0].enabled = active;
+    }
+
+    private void RagDollMake()
+    {
+        RagDollState(true);
+        AliveState(false);
+    }
+    private void AliveMake()
+    {
+        AliveState(true);
+        RagDollState(false);
+    }
+
+    private void Update()
+    {
+        lifeTime -= Time.deltaTime;
+        if (lifeTime <= 0)
+        {
+            RagDollMake();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            lifeTime = 5f;
+            AliveMake();
+        }
+    }
+}
