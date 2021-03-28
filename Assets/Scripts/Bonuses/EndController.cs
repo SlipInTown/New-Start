9a7c2740c3cd5a7e74498e1ab09f1f3dfc4b2825
Alexander <sashacomp2@gmail.com>
2021-03-28 15:02:51 +0300

    Revert "Revert "Task 2""
    This reverts commit da664c227b15e8ae2b7a3b624aa264ebb0fa0d72.

---
+++ Assets/Scripts/Bonuses/EndController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.SceneManagement;
+using static UnityEngine.Debug;
+
+namespace AlexSpace
+{
+    public class EndController
+    {
+        static int count = 0;
+        static readonly int EndValue = 3;
+        static readonly string SceneName = "New Start";
+        private static void CheckValue()
+        {
+            if (count >= EndValue)
+            {
+                count = 0;
+                SceneManager.LoadScene(SceneName);
+            }
+        }
+
+        public static void AddValue()
+        {
+            count++;
+            CheckValue();
+            Log($"У вас {count} очков, до победы {EndValue - count}");
+        }
+    }
+}
