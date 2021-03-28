47948963f2a37e8b6843dbb021b0bbdba90b2641
Alexander <sashacomp2@gmail.com>
2021-03-28 15:48:57 +0300

    ВтороеДЗ

---
+++ Assets/Scripts/Bonuses/BadBonus.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+namespace AlexSpace
+{
+    public sealed class BadBonus : MonoBehaviour, IBonus
+    {
+        [SerializeField] private float debuff = -2f;
+        private FirstPersonController link;
+        public void Effect(Collider other)
+        {
+            link = other.GetComponent<FirstPersonController>();
+            link.m_WalkSpeed += debuff;
+            link.m_RunSpeed += debuff * 2;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.CompareTag("Player")) return;
+            gameObject.SetActive(false);
+            Effect(other);
+        }
+    }
+}
