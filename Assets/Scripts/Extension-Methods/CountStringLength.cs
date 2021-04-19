3ef8c62b95dc246ff82f5407264b0a18e52d3c30
Alexander <sashacomp2@gmail.com>
2021-04-20 01:05:23 +0300

    Методы расширения и структуры данных

---
+++ Assets/Scripts/Extension-Methods/CountStringLength.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace AlexSpace
+{
+    public static class CountStringLength
+    {
+        public static void StringLength(this string abc)
+        {
+            Debug.Log($"Длина вашей строки = {abc.Length}");
+        }
+    }
+}
---
+++ Assets/Scripts/Extension-Methods/ListElementCount.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ListElementCount 
+{
+    public static Dictionary<T, int> ListElemCount<T>(this List<T> _list)
+    {
+        var tempDictionary = new Dictionary<T, int> { };
+
+        int count;
+        for (int i = 0; i < _list.Count; i++)
+        {
+            count = 0;
+            T value = _list[i];
+
+            for (int j = 0; j < _list.Count; j++)
+            {
+                if (_list[j].Equals(value))
+                {
+                    count++;
+                }
+            }
+
+            tempDictionary.Add(value, count);
+        }
+
+        return tempDictionary;
+    }
+}
